00f4771ea9075b18f61ff7fe0420e07a074f9a22
Thanhtnv <thanhtran190304@gmail.com>
2024-06-03 15:46:54 +0700

    done

--- Session03-OOP/FAP/StudentManagerV9/Entities/Student.cs
+++ Session03-OOP/FAP/StudentManagerV9/Entities/Student.cs
@@ -22,3 +22,19 @@ namespace StudentManagerV9.Entities
 
     }
 }
+
+//TỔNG KẾT: CÁCH THIẾT KẾ 1 CLASS LƯU TRỮ INFO CÁC OBJECT
+//CÁCH 1: KHAI BÁO CÁC _BACKING FIELD NHƯ BÊN JAVA
+//          SAU ĐÓ TẠO GET SET NHƯ BÊN JAVA
+//CÁCH 2: KHAI BÁO CÁC _BACKING FIELD NHƯ BÊN JAVA
+//          SAU ĐÓ LÀM HÀM GET SET THEO STYLE VIẾT GỘP
+//          VÀ XỬ LÝ TRÊN _BACKING FIELD, DÙNG THÊM EXPRESSION BODY NẾU CẦN
+//  public string Name { get => _name; set => _name = value;}
+//  * QUÊN CÚ PHÁP THÌ GÕ propfull + tab
+//  CÁCH NÀY ĐƯỢC GỌI LÀ FULL PROPERTY: HÀM + BACKING FIELD
+//CÁCH 3: KHAI BÁO HÀM GET SET THEO STYLE MỚI, NGẮN GỌN, KHÔNG THÈM XÀI _BACKING FIELD, RUNTIME SẼ TỰ GENERATE _BACKING FIELD
+//  public string Name { get; set; }
+// * QUÊN CÚ PHÁP THÌ GÕ prop + tab
+//  CÁCH NÀY ĐƯỢC GỌI LÀ AUTO-IMPLEMENTED PROPERTY
+//  (RUNTIME TỰ GENERATE RA _BACKING FIELD CHO TA)
+//CÁCH 4: KHAI BÁO _BACKING FIELD NHƯ BÊN JAVA VÀ CHỦ ĐỘNG VIẾT RIÊNG CÁC HÀM GET SET TRUYỀN THỐNG, HOẶC KHÔNG CÓ HÀM GET SET NHƯNG CÓ CÁC HÀM XỬ LÝ _BACKING FIELD
