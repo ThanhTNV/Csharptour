60be770e3f9c104e282cb7ab3b23a3d19770c87b
Thanhtnv <thanhtran190304@gmail.com>
2024-06-17 17:00:34 +0700

    configure EF Core done

--- Session06-Database/BookStore/Repositories/BookManagementDbContext.cs
+++ Session06-Database/BookStore/Repositories/BookManagementDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Repositories.Entities;
 
 namespace Repositories;
@@ -20,13 +21,25 @@ public partial class BookManagementDbContext : DbContext
 
     public virtual DbSet<Book> Books { get; set; }
 
+    //public virtual List<Book> _arrBooks;
+    //DbContext chính là class Cabinet, chứa 1 list các cuốn sách
+    //          _arrBooks.add(new Book() { });
     public virtual DbSet<BookCategory> BookCategories { get; set; }
 
     public virtual DbSet<UserAccount> UserAccounts { get; set; }
 
+    //HÀM ĐỌC FILE .JSON ĐỂ LẤY RA CONNECTION STRING
+    //ĐỀ THI CHO SẴN HÀM, CHỈ CẦN COPY AND PASTE
+    private string? GetConnectionString()
+    {
+        IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", true, true).Build();
+        return configuration["ConnectionStrings:DefaultConnectionStringDB"];
+        //                   "ConnectionStrings:DefaultConnectionStringDB"
+    }
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(local);Database= BookManagementDb;UID=sa;PWD=root;TrustServerCertificate=True");
+        => optionsBuilder.UseSqlServer(GetConnectionString());
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
